9ec48cda7b64566a13a49900f2f8295c3a322d63
Daniel Vicente <dluis2007@gmail.com>
2020-10-14 19:07:14 +0100

    Alterações no controler
    Signed-off-by: Daniel Vicente <dluis2007@gmail.com>

--- src/main/java/br/com/springboot/app/controller/UserController.java
+++ src/main/java/br/com/springboot/app/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.app.controller;
 
+import br.com.springboot.app.assembler.UserModelAssembler;
 import br.com.springboot.app.domain.User;
 import br.com.springboot.app.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,9 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -25,7 +26,9 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @AllArgsConstructor
 public class UserController {
 
-	private UserService service;
+	private final UserService service;
+	private final UserModelAssembler assembler;
+
 
 	@Operation(summary = "Find User by ID", responses = {
 			@ApiResponse(responseCode = "200", description = "Successful Find Users"),
@@ -33,7 +36,7 @@ public class UserController {
 			@ApiResponse(responseCode = "401", description = "Authentication Failure") })
 	@GetMapping(value = "/findById/{id}")
 	public EntityModel<User> findById(@PathVariable Long id) {
-		return service.findById(id);
+		return assembler.toModel(service.findById(id));
 	}
 
 
@@ -43,31 +46,36 @@ public class UserController {
 			@ApiResponse(responseCode = "401", description = "Authentication Failure") })
 	@GetMapping(value = "/")
 	public CollectionModel<EntityModel<User>> getUsers() {
-		List<EntityModel<User>> users = service.findAll();
+		List<EntityModel<User>> users = service.findAll().stream()
+				.map(assembler::toModel)
+				.collect(Collectors.toList());
 		return CollectionModel.of(users, linkTo(methodOn(UserController.class).getUsers()).withSelfRel());
 	}
 
 	@PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<EntityModel<User>> create(@RequestBody @Valid User user) {
-		EntityModel<User> userModel = service.save(user);
+		EntityModel<User> userModel = assembler.toModel(service.save(user));
 
+		return getEntityModelToResponseEntity(userModel);
+	}
+
+	@PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<EntityModel<User>> update(@RequestBody @Valid User user) {
+		EntityModel<User> userModel = assembler.toModel(service.update(user));
+		return getEntityModelToResponseEntity(userModel);
+	}
+
+
+	@DeleteMapping(value = "/inactivate/{id}")
+	public void inactivate(@PathVariable Long id) {
+		service.inactivate(id);
+	}
+
+	public ResponseEntity<EntityModel<User>> getEntityModelToResponseEntity(EntityModel<User> userModel) {
 		return ResponseEntity //
 				.created(userModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
 				.body(userModel);
 	}
 
-	@PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<EntityModel<User>> update(@RequestBody @Valid User user) {
-		EntityModel<User> userModel = service.update(user);
-		
-		return ResponseEntity //
-	      .created(userModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
-	      .body(userModel);
-	}
-
-	@DeleteMapping(value = "/inativate/{id}")
-	public void inativate(@PathVariable Long id) {
-		service.delete(id);
-	}
 
 }
--- src/main/java/br/com/springboot/app/service/UserService.java
+++ src/main/java/br/com/springboot/app/service/UserService.java
@@ -1,58 +1,48 @@
 package br.com.springboot.app.service;
 
 
-import br.com.springboot.app.assembler.UserModelAssembler;
 import br.com.springboot.app.domain.User;
 import br.com.springboot.app.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(propagation=Propagation.SUPPORTS)
+@AllArgsConstructor
 public class UserService 
 {
-	
-	@Autowired
-	private UserRepository repository;
+	private final UserRepository repository;
 
-	@Autowired
-	private UserModelAssembler assembler;
-	
-	public List<EntityModel<User>> findAll(){
-		return repository.findAll().stream()
-				.map(assembler::toModel)
-				.collect(Collectors.toList());
+	public List<User> findAll(){
+		return repository.findAll();
 	}
 
-	public EntityModel<User> findById(Long id) {
+	public User findById(Long id) {
+		return repository.findById(id).orElse(new User());
+	}
+//	@Transactional(propagation=Propagation.REQUIRED)
+//	public void delete(Long id) {
+//		repository.deleteById(id);
+//	}
+
+	@Transactional(propagation=Propagation.REQUIRED)
+	public User save(User user) {
+		return repository.save(user);
+	}
+
+	@Transactional(propagation=Propagation.REQUIRED)
+	public User update(User user) {
+		return repository.save(user) ;
+	}
+
+	@Transactional(propagation=Propagation.REQUIRED)
+	public void inactivate(Long id) {
 		User user = repository.findById(id).orElse(new User());
-		return assembler.toModel(user);
-	}
-	@Transactional(propagation=Propagation.REQUIRED)
-	public void delete(Long id) {
-		repository.deleteById(id);
-	}
-
-	@Transactional(propagation=Propagation.REQUIRED)
-	public EntityModel<User> save(User user) {
-		return assembler.toModel(repository.save(user));
-	}
-
-	@Transactional(propagation=Propagation.REQUIRED)
-	public EntityModel<User> update(User user) {
-		return assembler.toModel(repository.save(user));
-	}
-
-	@Transactional(propagation=Propagation.REQUIRED)
-	public void inativate(Long id) {
-		User user = repository.findById(id).orElse(new User());
-		repository.deleteById(id);
+		repository.deleteById(user.getId());
 	}
 
 }
