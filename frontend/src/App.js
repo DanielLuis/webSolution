ba5115184d37679c7ac51cfd79433e71a871cd97
Daniel Vicente <dluis2007@gmail.com>
2020-10-21 16:24:29 +0100

    configurações iniciais front end react
    Signed-off-by: Daniel Vicente <dluis2007@gmail.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserList from './UserList';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/users' exact={true} component={UserList}/>
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App;
